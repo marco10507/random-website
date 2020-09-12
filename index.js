d2de7926032f1580dc2c17a332a7586086ece1ff
marco10507 <marco10507@hotmail.com>
2020-09-12 23:30:00 +0200

    adding project

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get("/", (req, res)=>{
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get("/contact-form", (req, res)=>{
+  res.sendFile(path.join(__dirname, 'contact-form.html'));
+});
+
+
+app.listen(port, ()=>{
+  console.log(`Example app listening at http://localhost:${port}`)
+});
