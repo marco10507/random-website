11749be90d57ba648918c9ffa3933a0476866823
marco10507 <marco10507@hotmail.com>
2020-09-13 20:14:53 +0200

    adding more files

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require('path');
 const app = express();
 const port =  process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cases',  {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connected to mongoose database.");
+});
+
+const caseSchema = new mongoose.Schema({
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  email: {type: String, required: true},
+  description: {type: String, required: true},
+});
+
+const Case = mongoose.model('Case', caseSchema);
+
 
 app.use(express.static(`${__dirname}/public`));
 
@@ -13,6 +34,24 @@ app.get("/contact-form", (req, res)=>{
   res.sendFile(path.join(__dirname, 'contact-form.html'));
 });
 
+app.post("/new-case", (req, res)=>{
+  let caseData = Object.freeze({
+    firstName : req.body.firstName,
+    lastName : req.body.lastName,
+    email : req.body.email,
+    description : req.body.description,
+  });
+
+  const newCase = new Case(caseData);
+
+  newCase.save((error, newCase)=>{
+    if(error){
+      console.log("new case could not saved!!");
+    }
+
+    res.redirect("/");
+  });
+});
 
 app.listen(port, ()=>{
   console.log(`Example app listening at http://localhost:${port}`)
